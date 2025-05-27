6c6ffe01cdf4bddefde391a9da8270194d256c3a
alperozturk <alper_ozturk@proton.me>
2025-05-27 21:27:10 +0800

    Do not think different

--- src/blog/data/blog_data.js
+++ src/blog/data/blog_data.js
@@ -2,15 +2,10 @@ import {BlogType as PostType} from "./blog_type.js";
 
 export const blogData = [
   {
-    "title": "How JavaScript Works",
-    "summary": "Explore how JS runs in browsers and engines like V8.",
+    "title": "Do not think different. Listen users.",
+    "summary": "Used to be this words was much more powerful. Now it's just changed\n" +
+        "I don't see any difference. Let's rewind a bit and see what happened.",
     "type": PostType.Tech,
-    "path": "/assets/posts/tech/ThinkDifferent.md"
+    "path": "/assets/posts/tech/DoNotThinkDifferent.md"
   },
-  {
-    "title": "What I Learned from Solo Travel",
-    "summary": "Lessons and experiences from traveling alone.",
-    "type": PostType.Swift,
-    "path": "/assets/posts/swift/MainActor.md"
-  }
 ]
