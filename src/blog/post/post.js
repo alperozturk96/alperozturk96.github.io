6ac9145664bd89e9f398d0545bc6ec3217d818c4
alperozturk <alper_ozturk@proton.me>
2025-05-30 10:43:22 +0800

    add copy button

--- src/blog/post/post.js
+++ src/blog/post/post.js
@@ -9,7 +9,6 @@ const post = blogData.find(p => p.path === path);
 if (!post) {
     contentEl.textContent = "Post not found.";
 } else {
-    setCopyButtonEvents();
     document.title = post.title;
 
     fetch(post.path)
@@ -25,6 +24,7 @@ if (!post) {
             contentEl.appendChild(postBody);
 
             Prism.highlightAll();
+            setCopyButtonEvents();
         })
         .catch(error => {
             contentEl.textContent = "Error loading post: " + error.message;
@@ -39,12 +39,9 @@ function setCopyButtonEvents() {
 
             await navigator.clipboard.writeText(code.innerText);
 
-            const originalIcon = button.innerHTML;
-            button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="copy-icon" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M13.485 1.929a1 1 0 0 1 1.415 1.414l-7.778 7.778a1 1 0 0 1-1.414 0L1.1 7.929a1 1 0 1 1 1.414-1.414L6.414 10.1l7.071-7.071z"/>
-            </svg>`;
-
-            setTimeout(() => button.innerHTML = originalIcon, 1500);
+            const originalText = button.innerText;
+            button.innerText = "Copied";
+            setTimeout(() => button.innerText = originalText, 1500);
         });
     });
 }
