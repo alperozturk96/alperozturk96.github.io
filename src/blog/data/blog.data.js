b8522ea8013090dac49f2777691cc964f1e343cf
alperozturk <alper_ozturk@proton.me>
2025-06-19 11:34:02 +0200

    move to posts

--- src/blog/data/blog.data.js
+++ src/blog/data/blog.data.js
@@ -5,35 +5,35 @@ export const blogData = [
     "title": "Do not think different. Listen users.",
     "summary": "Think Different. Those words used to mean something powerful. Now, they feel hollow. What changed? Let’s rewind and take a closer look at how Apple lost its way and what it needs to do to reconnect with real user needs.",
     "type": PostType.Tech,
-    "path": "/assets/posts/tech/Do Not Think Different.html",
+    "path": "../../blog/post/tech/Do Not Think Different.html",
     "date": "24 May 2025"
   },
   {
     "title": "Customizing the Navigation Bar in SwiftUI",
     "summary": "Let’s take a quick look at why customizing SwiftUI’s navigation bar isn’t as simple as it should be and how you can fix it with a clean, reusable solution that works across iOS versions. Despite improvements in iOS 16, full control still needs some UIKit magic.",
     "type": PostType.Swift,
-    "path": "/assets/posts/swift/Customizing the Navigation Bar in SwiftUI.html",
+    "path": "../../blog/post/swift/Customizing the Navigation Bar in SwiftUI.html",
     "date": "30 May 2025"
   },
   {
     "title": "Migrating from Gson to Kotlinx Serialization",
     "summary": "Migrating from Gson to Kotlinx Serialization for easier and faster JSON handling in Android apps. Let’s dive into the story ...",
     "type": PostType.Kotlin,
-    "path": "/assets/posts/kotlin/Migrating from Gson to Kotlinx Serialization.html",
+    "path": "../../blog/post/kotlin/Migrating from Gson to Kotlinx Serialization.html",
     "date": "1 June 2025"
   },
   {
     "title": "Concurrency",
     "summary": "You've probably heard this hundreds of times before, but let me explain also ...",
     "type": PostType.ComputerScience,
-    "path": "/assets/posts/computerScience/concurrency/Concurrency.html",
+    "path": "../../blog/post/computerScience/concurrency/Concurrency.html",
     "date": "14 June 2025"
   },
   {
     "title": "UIKit Dynamics",
     "summary": "Hidden gem of UIKit since iOS 7.",
     "type": PostType.Swift,
-    "path": "/assets/posts/swift/uiKitDynamics/UIKit Dynamics.html",
+    "path": "../../blog/post/swift/uiKitDynamics/UIKit Dynamics.html",
     "date": "15 June 2025"
   },
 ]
