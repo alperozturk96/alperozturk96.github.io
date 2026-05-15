f64c166c4a66a3e954e84ccfce02704101068069
alperozturk96 <alper_ozturk@proton.me>
2026-05-15 18:01:24 +0200

    wip

--- blog/data/blog.data.js
+++ blog/data/blog.data.js
@@ -1,179 +1,196 @@
-import {BlogType as PostType} from "./blog.type.js";
+import { BlogType as PostType } from "./blog.type.js";
 
 export const blogData = [
-    {
-        "title": "Customizing the Navigation Bar in SwiftUI",
-        "summary": "Let’s take a quick look at why customizing SwiftUI’s navigation bar isn’t as simple as it should be and how you can fix it with a clean, reusable solution that works across iOS versions. Despite improvements in iOS 16, full control still needs some UIKit magic.",
-        "type": PostType.iOS,
-        "path": "customizing-the-navigation-bar-in-swiftui",
-        "date": "30 May 2025"
-    },
-    {
-        "title": "Migrating from Gson to Kotlinx Serialization",
-        "summary": "Migrating from Gson to Kotlinx Serialization for easier and faster JSON handling in Android apps. Let’s dive into the story ...",
-        "type": PostType.Android,
-        "path": "migrating-from-gson-to-kotlinx-serialization",
-        "date": "1 June 2025"
-    },
-    {
-        "title": "Concurrency",
-        "summary": "You've probably heard this hundreds of times before, but let me explain also ...",
-        "type": PostType.ComputerScience,
-        "path": "concurrency",
-        "date": "14 June 2025"
-    },
-    {
-        "title": "UIKit Dynamics",
-        "summary": "Hidden gem of UIKit since iOS 7.",
-        "type": PostType.iOS,
-        "path": "uikit-dynamics",
-        "date": "15 June 2025"
-    },
-    {
-        "title": "Managing App Lifecycle with UIApplicationDelegate & UISceneDelegate",
-        "summary": "",
-        "type": PostType.iOS,
-        "path": "managing-app-life-cycle",
-        "date": "22 June 2025"
-    },
-    {
-        "title": "SwiftUI Navigation Problems",
-        "summary": "",
-        "type": PostType.iOS,
-        "path": "swiftui-navigation-problems",
-        "date": "05 July 2025"
-    },
-    {
-        "title": "Kotlin Flows",
-        "summary": "",
-        "type": PostType.Android,
-        "path": "kotlin-flows",
-        "date": "13 July 2025"
-    },
-    {
-        "title": "End-to-End Encryption with CryptoKit",
-        "summary": "Using Apple’s CryptoKit, this post demonstrates how to implement end-to-end encryption with Diffie-Hellman key exchange to securely transmit data over an insecure channel. It also includes digital signature verification to ensure the authenticity and integrity of the messages.",
-        "type": PostType.iOS,
-        "path": "end-to-end-encryption-with-crypto-kit",
-        "date": "19 July 2025"
-    },
-    {
-        "title": "Using A Locally Hosted Large Language Model with MapKit",
-        "summary": "",
-        "type": PostType.iOS,
-        "path": "using-language-model-with-map-kit",
-        "date": "27 July 2025"
-    },
-    {
-        "title": "Concurrency with Swift 6",
-        "summary": "",
-        "type": PostType.iOS,
-        "path": "concurrency-with-swift-6",
-        "date": "02 August 2025"
-    },
-    {
-        "title": "Android runtime and Dalvik",
-        "summary": "",
-        "type": PostType.Android,
-        "path": "android-runtime-and-dalvik",
-        "date": "10 August 2025"
-    },
-    {
-        "title": "GraphQL & Apollo Client",
-        "summary": "",
-        "type": PostType.iOS,
-        "path": "graphql-and-apollo-client",
-        "date": "24 August 2025"
-    },
-    {
-        "title": "Integrating an Interval Timer into the Music Player",
-        "summary": "",
-        "type": PostType.Android,
-        "path": "integrating-an-interval-timer-into-the-music-player",
-        "date": "31 August 2025"
-    },
-    {
-        "title": "Creating a Launcher with Tile Layouts",
-        "summary": "",
-        "type": PostType.Android,
-        "path": "creating-a-launcher-with-tile-layouts",
-        "date": "7 September 2025"
-    },
-    {
-        "title": "Automatic Reference Counting in Swift",
-        "summary": "In this blogpost you can learn about ARC, strong, weak, unowned references, memory leaks, and common memory leak scenarios.",
-        "type": PostType.iOS,
-        "path": "automatic-reference-counting-in-swift",
-        "date": "3 October 2025"
-    },
-    {
-        "title": "Understanding FileManager API",
-        "summary": "Lets explore, one of the most powerful and essential tools provided by the Foundation framework.",
-        "type": PostType.iOS,
-        "path": "understanding-file-manager-api",
-        "date": "18 October 2025"
-    },
-    {
-        "title": "Composition over Inheritance",
-        "summary": "Understanding when to use inheritance and when to favor composition can define how maintainable your codebase becomes.",
-        "type": PostType.ComputerScience,
-        "path": "composition-over-inheritance",
-        "date": "26 October 2025"
-    },
-    {
-        "title": "Fail Fast and Don't Be Afraid",
-        "summary": "Design software to catch critical issues early.",
-        "type": PostType.ComputerScience,
-        "path": "fail-fast-and-dont-be-afraid",
-        "date": "01 November 2025"
-    },
-    {
-        "title": "Creating PDFs Using UIGraphicsPDFRenderer",
-        "summary": "",
-        "type": PostType.iOS,
-        "path": "create-pdf-with-swift",
-        "date": "08 November 2025"
-    },
-    {
-        "title": "Model-View-ViewModel Architectural Pattern",
-        "summary": "What is MVVM, how to implement it in mobile apps, and the common challenges and solutions for building maintainable, testable, and scalable applications.",
-        "type": PostType.ComputerScience,
-        "path": "mvvm",
-        "date": "15 November 2025"
-    },
-    {
-        "title": "Side-by-Side Implementation: Creating a List",
-        "summary": "You can learn how to create lists using XML and Jetpack Compose on Android, as well as UIKit and SwiftUI on iOS.",
-        "type": [PostType.Android, PostType.iOS],
-        "path": "side-by-side-implementation-creating-a-list",
-        "date": "22 November 2025"
-    },
-    {
-        "title": "Pipeline Pattern",
-        "summary": "",
-        "type": PostType.ComputerScience,
-        "path": "pipeline-pattern",
-        "date": "06 December 2025"
-    },
-    {
-        "title": "Strategy Pattern",
-        "summary": "",
-        "type": PostType.ComputerScience,
-        "path": "strategy-pattern",
-        "date": "03 January 2026"
-    },
-    {
-        "title": "Bloom Filter",
-        "summary": "",
-        "type": PostType.ComputerScience,
-        "path": "bloom-filter",
-        "date": "15 February 2026"
-    },
-    {
-        "title": "Core Animation CATransform3D",
-        "summary": "",
-        "type": PostType.iOS,
-        "path": "core-animation-ca-transform-3d",
-        "date": "22 March 2026"
-    },
-]
+  {
+    title: "Customizing the Navigation Bar in SwiftUI",
+    summary:
+      "Let’s take a quick look at why customizing SwiftUI’s navigation bar isn’t as simple as it should be and how you can fix it with a clean, reusable solution that works across iOS versions. Despite improvements in iOS 16, full control still needs some UIKit magic.",
+    type: PostType.iOS,
+    path: "customizing-the-navigation-bar-in-swiftui",
+    date: "30 May 2025",
+  },
+  {
+    title: "Migrating from Gson to Kotlinx Serialization",
+    summary:
+      "Migrating from Gson to Kotlinx Serialization for easier and faster JSON handling in Android apps. Let’s dive into the story ...",
+    type: PostType.Android,
+    path: "migrating-from-gson-to-kotlinx-serialization",
+    date: "1 June 2025",
+  },
+  {
+    title: "Concurrency",
+    summary:
+      "You've probably heard this hundreds of times before, but let me explain also ...",
+    type: PostType.ComputerScience,
+    path: "concurrency",
+    date: "14 June 2025",
+  },
+  {
+    title: "UIKit Dynamics",
+    summary: "Hidden gem of UIKit since iOS 7.",
+    type: PostType.iOS,
+    path: "uikit-dynamics",
+    date: "15 June 2025",
+  },
+  {
+    title:
+      "Managing App Lifecycle with UIApplicationDelegate & UISceneDelegate",
+    summary: "",
+    type: PostType.iOS,
+    path: "managing-app-life-cycle",
+    date: "22 June 2025",
+  },
+  {
+    title: "SwiftUI Navigation Problems",
+    summary: "",
+    type: PostType.iOS,
+    path: "swiftui-navigation-problems",
+    date: "05 July 2025",
+  },
+  {
+    title: "Kotlin Flows",
+    summary: "",
+    type: PostType.Android,
+    path: "kotlin-flows",
+    date: "13 July 2025",
+  },
+  {
+    title: "End-to-End Encryption with CryptoKit",
+    summary:
+      "Using Apple’s CryptoKit, this post demonstrates how to implement end-to-end encryption with Diffie-Hellman key exchange to securely transmit data over an insecure channel. It also includes digital signature verification to ensure the authenticity and integrity of the messages.",
+    type: PostType.iOS,
+    path: "end-to-end-encryption-with-crypto-kit",
+    date: "19 July 2025",
+  },
+  {
+    title: "Using A Locally Hosted Large Language Model with MapKit",
+    summary: "",
+    type: PostType.iOS,
+    path: "using-language-model-with-map-kit",
+    date: "27 July 2025",
+  },
+  {
+    title: "Concurrency with Swift 6",
+    summary: "",
+    type: PostType.iOS,
+    path: "concurrency-with-swift-6",
+    date: "02 August 2025",
+  },
+  {
+    title: "Android runtime and Dalvik",
+    summary: "",
+    type: PostType.Android,
+    path: "android-runtime-and-dalvik",
+    date: "10 August 2025",
+  },
+  {
+    title: "GraphQL & Apollo Client",
+    summary: "",
+    type: PostType.iOS,
+    path: "graphql-and-apollo-client",
+    date: "24 August 2025",
+  },
+  {
+    title: "Integrating an Interval Timer into the Music Player",
+    summary: "",
+    type: PostType.Android,
+    path: "integrating-an-interval-timer-into-the-music-player",
+    date: "31 August 2025",
+  },
+  {
+    title: "Creating a Launcher with Tile Layouts",
+    summary: "",
+    type: PostType.Android,
+    path: "creating-a-launcher-with-tile-layouts",
+    date: "7 September 2025",
+  },
+  {
+    title: "Automatic Reference Counting in Swift",
+    summary:
+      "In this blogpost you can learn about ARC, strong, weak, unowned references, memory leaks, and common memory leak scenarios.",
+    type: PostType.iOS,
+    path: "automatic-reference-counting-in-swift",
+    date: "3 October 2025",
+  },
+  {
+    title: "Understanding FileManager API",
+    summary:
+      "Lets explore, one of the most powerful and essential tools provided by the Foundation framework.",
+    type: PostType.iOS,
+    path: "understanding-file-manager-api",
+    date: "18 October 2025",
+  },
+  {
+    title: "Composition over Inheritance",
+    summary:
+      "Understanding when to use inheritance and when to favor composition can define how maintainable your codebase becomes.",
+    type: PostType.ComputerScience,
+    path: "composition-over-inheritance",
+    date: "26 October 2025",
+  },
+  {
+    title: "Fail Fast and Don't Be Afraid",
+    summary: "Design software to catch critical issues early.",
+    type: PostType.ComputerScience,
+    path: "fail-fast-and-dont-be-afraid",
+    date: "01 November 2025",
+  },
+  {
+    title: "Creating PDFs Using UIGraphicsPDFRenderer",
+    summary: "",
+    type: PostType.iOS,
+    path: "create-pdf-with-swift",
+    date: "08 November 2025",
+  },
+  {
+    title: "Model-View-ViewModel Architectural Pattern",
+    summary:
+      "What is MVVM, how to implement it in mobile apps, and the common challenges and solutions for building maintainable, testable, and scalable applications.",
+    type: PostType.ComputerScience,
+    path: "mvvm",
+    date: "15 November 2025",
+  },
+  {
+    title: "Side-by-Side Implementation: Creating a List",
+    summary:
+      "You can learn how to create lists using XML and Jetpack Compose on Android, as well as UIKit and SwiftUI on iOS.",
+    type: [PostType.Android, PostType.iOS],
+    path: "side-by-side-implementation-creating-a-list",
+    date: "22 November 2025",
+  },
+  {
+    title: "Pipeline Pattern",
+    summary: "",
+    type: PostType.ComputerScience,
+    path: "pipeline-pattern",
+    date: "06 December 2025",
+  },
+  {
+    title: "Strategy Pattern",
+    summary: "",
+    type: PostType.ComputerScience,
+    path: "strategy-pattern",
+    date: "03 January 2026",
+  },
+  {
+    title: "Bloom Filter",
+    summary: "",
+    type: PostType.ComputerScience,
+    path: "bloom-filter",
+    date: "15 February 2026",
+  },
+  {
+    title: "Core Animation CATransform3D",
+    summary: "",
+    type: PostType.iOS,
+    path: "core-animation-ca-transform-3d",
+    date: "22 March 2026",
+  },
+  {
+    title: "Retrieving Metadata of an Audio File in Swift",
+    summary: "",
+    type: PostType.iOS,
+    path: "retrieving-metadata-in-swift",
+    date: "15 May 2026",
+  },
+];
